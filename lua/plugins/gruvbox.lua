56cfa2e8b0e4b1bb9d06a18e3de07d146a3ffdc2
Mithlesh.Kumar <Mithlesh.Kumar@ascendlearning.com>
2023-04-19 21:05:34 +0530

    added rose-pine and gruvbox with transparent

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -2,10 +2,32 @@ return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
   -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  "LazyVim/LazyVim",
+  opts = {
+    style = "main",
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
     },
   },
+
+  config = function(_, opts)
+    require("gruvbox").setup(opts) -- opts here are passed from above
+    require("notify").setup({
+      background_colour = "#000000",
+    })
+    local colors = require("gruvbox.colors").setup({})
+    vim.cmd("highlight WinSeparator guifg=" .. colors.bg_highlight)
+    vim.cmd([[colorscheme gruvbox]])
+  end,
+
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "gruvbox",
+  --   },
+  -- },
 }
--- lua/plugins/rosepine.lua
+++ lua/plugins/rosepine.lua
@@ -1,11 +1,33 @@
 return {
-  -- add gruvbox
+  -- add rose-pine
   { "rose-pine/neovim" },
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "rose-pine",
+  -- Configure LazyVim to load rose-pine
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  "LazyVim/LazyVim",
+  opts = {
+    style = "main",
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
     },
   },
+
+  config = function(_, opts)
+    require("rose-pine").setup(opts) -- opts here are passed from above
+    require("notify").setup({
+      background_colour = "#000000",
+    })
+    local colors = require("rose-pine.colors").setup({})
+    vim.cmd("highlight WinSeparator guifg=" .. colors.bg_highlight)
+    vim.cmd([[colorscheme rose-pine]])
+  end,
+
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "rose-pine",
+  --   },
+  -- },
 }
